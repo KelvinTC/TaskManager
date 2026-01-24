e12140127f9da16339c05c0bd35702c54ea5b23b
Chisanga <kelvintchisanga@gmail.com>
2026-01-24 12:18:44 +0200

    Queue UserInvited notification to prevent timeout on invite

--- app/Notifications/UserInvited.php
+++ app/Notifications/UserInvited.php
@@ -4,10 +4,11 @@ namespace App\Notifications;
 
 use App\Channels\WhatsappChannel;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class UserInvited extends Notification
+class UserInvited extends Notification implements ShouldQueue
 {
     use Queueable;
 
